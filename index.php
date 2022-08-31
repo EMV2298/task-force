d9c82394766157a2d7a2f9993608417a66e98bc1
Евгений <emv2298@icloud.com>
2022-08-31 14:59:41 +0300

    test

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 require_once "vendor/autoload.php";
 use taskforce\business\Task;
 
-$executor = NULL;
-
-$task1 = new Task(1, $executor, 'new');
+$task1 = new Task(1, NULL, 'new');
 
 var_dump($task1->getAvailableActions(1));
-
+var_dump($task1->getNextStatus('cancel'));
+var_dump(new Task(1, NULL, 'new'));
