36a420dff8b7eef651af37111835eb8262a03bb1
Евгений <emv2298@icloud.com>
2022-09-23 17:46:34 +0300

    fix filter form

--- views/tasks/tasks.php
+++ views/tasks/tasks.php
@@ -53,36 +53,31 @@ $this->title = "Taskforce";
 </div>
 <div class="right-column">
     <div class="right-card black">
-
-        <?php $form = ActiveForm::begin(
-            [
+        <div class="search-form">
+            <h4 class="head-card">Категории</h4>
+            
+            <?php $form = ActiveForm::begin([
                 'id' => 'filterTasks',
-                'options' => ['class' => 'search-form'],
-                'fieldConfig' => [
-                    'template' => '{input}{error}'
-                ]
-            ]
-        ); ?>
+                'fieldConfig' => ['template' => '{input}']
+            ]);
 
-        <h4 class="head-card">Категории</h4>
+            echo $form->field($model, 'categories')
+                ->checkboxList(Categories::getCategories(), ['class' => 'checkbox-wrapper control-label']); ?>
 
-        <?php echo $form->field($model, 'categories')
-            ->checkboxList(Categories::getCategories(), ['class' => 'checkbox-wrapper control-label']); ?>
+            <h4 class="head-card">Дополнительно</h4>
 
-        <h4 class="head-card">Дополнительно</h4>
+            <?php
+            echo $form->field($model, 'noOffer')->checkbox(['labelOptions' => ['class' => 'control-label',]]);
+            echo $form->field($model, 'isDistant')->checkbox(['labelOptions' => ['class' => 'control-label',]]);
+            ?>
 
-        <?php
-         echo $form->field($model, 'noOffer')->checkbox(['labelOptions' => ['class' => 'control-label',]]);
-         echo $form->field($model, 'isDistant')->checkbox(['labelOptions' => ['class' => 'control-label',]]);
-        ?>    
+            <h4 class="head-card">Период</h4>
 
-        <h4 class="head-card">Период</h4>
+            <?php echo $form->field($model, 'period')->dropDownList(FilterTasks::PERIODS); ?>
 
-        <?php echo $form->field($model, 'period')->dropDownList(FilterTasks::PERIODS); ?>
+            <input type="submit" class="button button--blue" value="Искать">
 
-        <input type="submit" class="button button--blue" value="Искать">
-
-        <?php ActiveForm::end(); ?>
-
+            <?php ActiveForm::end(); ?>
+        </div>
     </div>
 </div>
