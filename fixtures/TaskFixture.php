3201464bdb53dbaa6b3ee7f48286a17959166192
Евгений <emv2298@icloud.com>
2022-09-27 02:27:21 +0300

    change fixtures

--- fixtures/TaskFixture.php
+++ fixtures/TaskFixture.php
@@ -7,5 +7,5 @@ use yii\test\ActiveFixture;
 class TaskFixture extends ActiveFixture
 {
     public $modelClass = 'app\models\Tasks';
-
+    public $depends = ['app\fixtures\UserFixture'];
 }
--- fixtures/templates/users.php
+++ fixtures/templates/users.php
@@ -3,11 +3,12 @@ $faker = Faker\Factory::create('ru_RU');
 return [
   'name' => $faker->firstName(),
   'email' => $faker->email(),
-  'dob' => $faker->date(),
+  'dob' => $faker->date('Y-m-d', '2000-12-30'),
   'password' => '12345',
   'phonenumber' => $faker->phoneNumber(),
   'city_id' => random_int(1, 10),
   'description' => $faker->realText(),
   'status' => random_int(0,1),
   'show_contacts' => random_int(0,1),
+  'avatar' => '/img/man-glasses.png',
 ];
