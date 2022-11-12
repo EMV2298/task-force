ed911a85be4c43482888926af0bf49e7f0a1cef5
Евгений <emv2298@icloud.com>
2022-11-12 22:56:10 +0300

    php doc

--- src/Files.php
+++ src/Files.php
@@ -4,6 +4,11 @@ namespace taskforce;
 
 class Files 
 {
+   /**
+   * Загружает файлы на сервер
+   * @param array $files Файлы для загрузки
+   * @return array Возвращает имена загруженных файлов     
+   */
   public static function uploadTaskFiles(array $files): array
   {
     $savedFiles = [];    
@@ -17,6 +22,11 @@ class Files
     return $savedFiles;    
   }
 
+   /**
+   * Загружает файл на сервер
+   * @param object $file Файл для загрузки
+   * @return string Возвращает имя загруженного файла  
+   */
   public static function uploadUserAvatar(object $file): string
   {
     $name = uniqid('user-avatar') . '.' . $file->getExtension();
@@ -24,6 +34,11 @@ class Files
     return $file->saveAs('@webroot/uploads/user-avatar/' . $name) ? $name : '';  
   }
 
+   /**
+   * Загружает файлы на сервер
+   * @param string $filesUrl Url файла для загрузки
+   * @return string Возвращает имя загруженного файла     
+   */
   public static function uploadUrlAvatar(string $filesUrl): string
   {
     $name = uniqid('user') . '.png';  
